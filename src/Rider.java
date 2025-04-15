ca58c9df9ee83751b766e7cbf0f4437b966dee91
Prachi Jindal <prachibansal135@gmail.com>
2025-04-15 15:03:18 +0200

    implemented rider class with fields, methods and constructors

---
+++ src/Rider.java
@@ -0,0 +1,35 @@
+import javax.xml.namespace.QName;
+
+public class Rider {
+
+    private String name;
+    private String riderId;
+    private String location;
+
+    static int riderCount = 1;
+
+    private Rider() {
+    }
+
+    private Rider(String name, String location) {
+        this.name = name;
+        this.location = location;
+        this.riderId = generateRiderId();
+    }
+
+    private String generateRiderId() {
+        return "RD" + riderCount++;
+    }
+
+    public static Rider createRider(String name, String location){
+        return new Rider(name, location);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+}
